977d6ba8e1c3ff59bc99f5ae232019bd3776c8fd
sambeevors <sam.beevors@parall.ax>
2018-10-18 15:50:19 +0100

    Added serveral more jQuery-like functions

--- src/pumpkin.js
+++ src/pumpkin.js
@@ -32,8 +32,53 @@ const pumpkin = (() => {
   m.last = array => array[array.length - 1]
 
   /**
+   * Get the children of an element
+   * @param  {Node} el The element to search
+   * @return {Array}   The children of the passed element
+   */
+  m.children = el => {
+    let children = []
+    for (let i = el.children.length; i--; ) {
+      if (el.children[i].nodeType != 8) children.unshift(el.children[i])
+    }
+    return children
+  }
+
+  /**
+   * Get the siblings of an element
+   * @param  {Node} el The element to search
+   * @return {Array}   The siblings of the passed element
+   */
+  m.siblings = el => {
+    let siblings = Array.prototype.slice.call(el.parentNode.children)
+    for (let i = siblings.length; i--; ) {
+      if (siblings[i] === el) {
+        siblings.splice(i, 1)
+        break
+      }
+    }
+    return siblings
+  }
+
+  /**
+   * Check if element contains child
+   * @param  {Node} el    The element to search
+   * @param  {Node} child The child element to search for
+   * @return {Boolean}    Whether or not child was found
+   */
+  m.contains = (el, child) => el !== child && el.contains(child)
+
+  /**
+   * Removes all children from element
+   * @param {Node} el The element to empty
+   */
+  m.empty = el => {
+    while (el.firstChild) el.removeChild(el.firstChild)
+  }
+
+  /**
    * Run callback function once DOM has fully loaded
-   * @param  {Function} cb The function to run once ready
+   * @param {Function} cb The function to run once ready
    */
   m.ready = cb => {
     if (document.readyState != 'loading') cb()
@@ -44,6 +89,45 @@ const pumpkin = (() => {
         if (document.readyState == 'complete') cb()
       })
   }
+
+  /**
+   * Show selected element
+   * @param {Node} el The element to show
+   */
+  m.show = el => {
+    el.style.display = ''
+  }
+
+  /**
+   * Hide selected element
+   * @param {Node} el The element to hide
+   */
+  m.hide = el => {
+    el.style.display = 'none'
+  }
+
+  /**
+   * Toggle visibility of selected element
+   * @param {Node} el The element to toggle
+   */
+  m.toggle = el => {
+    el.style.display = el.style.display == 'none' ? '' : 'none'
+  }
+
+  /**
+   * Remove element from the DOM
+   * @param {Node} el The element to remove
+   */
+  m.remove = el => {
+    el.parentNode.removeChild(el)
+  }
+
+  /**
+   * Trim whitespace from a string
+   * @param  {String} string The string to trim
+   * @return {String}        The trimmed string
+   */
+  m.trim = string => string.replace(/^\s+|\s+$/g, '')
 
   return m
 })()

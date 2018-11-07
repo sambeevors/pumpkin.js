f7d82dca34f4ebbf25be4cefceffd252e6f45dd3
sambeevors <sam.beevors@parall.ax>
2018-11-07 16:17:49 +0000

    Added count and reverse parameters to first and last functions, allowing the user to get first/last x items, and optionally return this array in reverse order

--- src/pumpkin.js
+++ src/pumpkin.js
@@ -17,18 +17,34 @@ export const qsa = (selector, scope = document) => [
 ]
 
 /**
- * Get the first element in an array
- * @param  {Array} array The elements tomselect from
- * @return {Node}        The first element in the array
+ * Get the first item(s) in an array
+ * @param  {Array} array     The array to select from
+ * @param  {Number} count    The number of items to retrieve (optional)
+ * @param  {Boolean} reverse Whether to reverse the returned array (optional)
+ * @return {Mixed}           Either first item or an array of first x items
  */
-export const first = array => array[0]
+export const first = (array, count, reverse) => {
+  if (!count) return array[0]
+
+  let newArray = []
+  for (let i = 0; i < count; i++) newArray.push(array[i])
+  return reverse ? newArray.reverse() : newArray
+}
 
 /**
- * Get the last element in an array
- * @param  {Array} array The elements to select from
- * @return {Node}        The last element in the array
+ * Get the last item(s) in an array
+ * @param  {Array} array     The array to select from
+ * @param  {Number} count    The number of items to retrieve (optional)
+ * @param  {Boolean} reverse Whether to reverse the returned array (optional)
+ * @return {Mixed}           Either last item or an array of last x items
  */
-export const last = array => array[array.length - 1]
+export const last = (array, count, reverse) => {
+  if (!count) return array[array.length - 1]
+
+  let newArray = []
+  for (let i = 1; i <= count; i++) newArray.push(array[array.length - i])
+  return reverse ? newArray : newArray.reverse()
+}
 
 /**
  * Get the children of an element

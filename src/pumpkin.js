e1f805f7abbc334803fd0d0cd92dbf0d07bac437
sambeevors <sam.beevors@parall.ax>
2018-11-21 15:30:47 +0000

    Added slugify function

--- src/pumpkin.js
+++ src/pumpkin.js
@@ -19,8 +19,8 @@ export const qsa = (selector, scope = document) => [
 /**
  * Get the first item(s) in an array
  * @param  {Array} array     The array to select from
- * @param  {Number} count    The number of items to retrieve (optional)
- * @param  {Boolean} reverse Whether to reverse the returned array (optional)
+ * @param  {Number} count    The number of items to retrieve [optional]
+ * @param  {Boolean} reverse Whether to reverse the returned array [optional]
  * @return {Any}             Either first item or an array of first x items
  */
 export const first = (array, count, reverse) => {
@@ -34,8 +34,8 @@ export const first = (array, count, reverse) => {
 /**
  * Get the last item(s) in an array
  * @param  {Array} array     The array to select from
- * @param  {Number} count    The number of items to retrieve (optional)
- * @param  {Boolean} reverse Whether to reverse the returned array (optional)
+ * @param  {Number} count    The number of items to retrieve [optional]
+ * @param  {Boolean} reverse Whether to reverse the returned array [optional]
  * @return {Any}             Either last item or an array of last x items
  */
 export const last = (array, count, reverse) => {
@@ -192,4 +192,65 @@ export const ready = cb => {
     document.attachEvent('onreadystatechange', () => {
       if (document.readyState == 'complete') cb()
     })
+}
+
+/**
+ * Slugify a string
+ * @param {String} text The string to slugify
+ * @param {String} text Custom word seperator [optional]
+ * @return {String}     The 'slugified' string
+ */
+export const slugify = (text, separator) => {
+  text = text
+    .toString()
+    .toLowerCase()
+    .trim()
+
+  const sets = [
+    { to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶ]' },
+    { to: 'c', from: '[ÇĆĈČ]' },
+    { to: 'd', from: '[ÐĎĐÞ]' },
+    { to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]' },
+    { to: 'g', from: '[ĜĞĢǴ]' },
+    { to: 'h', from: '[ĤḦ]' },
+    { to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]' },
+    { to: 'j', from: '[Ĵ]' },
+    { to: 'ij', from: '[Ĳ]' },
+    { to: 'k', from: '[Ķ]' },
+    { to: 'l', from: '[ĹĻĽŁ]' },
+    { to: 'm', from: '[Ḿ]' },
+    { to: 'n', from: '[ÑŃŅŇ]' },
+    { to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]' },
+    { to: 'oe', from: '[Œ]' },
+    { to: 'p', from: '[ṕ]' },
+    { to: 'r', from: '[ŔŖŘ]' },
+    { to: 's', from: '[ßŚŜŞŠ]' },
+    { to: 't', from: '[ŢŤ]' },
+    { to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]' },
+    { to: 'w', from: '[ẂŴẀẄ]' },
+    { to: 'x', from: '[ẍ]' },
+    { to: 'y', from: '[ÝŶŸỲỴỶỸ]' },
+    { to: 'z', from: '[ŹŻŽ]' },
+    { to: '-', from: "[·/_,:;']" }
+  ]
+
+  sets.forEach(set => {
+    text = text.replace(new RegExp(set.from, 'gi'), set.to)
+  })
+
+  text = text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+
+  if (typeof separator !== 'undefined' && separator !== '-') {
+    text = text.replace(/-/g, separator)
+  }
+
+  return text
 }

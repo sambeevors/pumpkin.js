df46a66232829ac5651416d61e3637d8e306a474
sambeevors <sam.beevors@parall.ax>
2019-01-08 17:37:19 +0000

    Added algolia search

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,10 @@ module.exports = {
   servceWorker: true,
   ga: 'UA-128293838-1',
   themeConfig: {
+    algolia: {
+      apiKey: 'a99a0ed2c5ff3933253ee3931dcf0e88',
+      indexName: 'pumpkin docs'
+    },
     nav: [
       { text: 'Getting Started', link: '/guide/' },
       { text: 'Functions', link: '/functions/' },
